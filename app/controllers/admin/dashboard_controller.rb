46e845cba87e5d38c725320a0ead1585a98e2d4a
Viktor Ristic <vktr.r2@protonmail.com>
2023-06-01 15:51:32 -0400

    Add counts data to Admin Dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,17 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASSWORD']
+
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
+
+
+=begin
+
+Display a count of how many products are in the database
+Display a count of how many categories are in the database
+
+=end
