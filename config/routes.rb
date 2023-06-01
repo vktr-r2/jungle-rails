42b2428f17073847e22fe50110a57eabde902026
Viktor Ristic <vktr.r2@protonmail.com>
2023-06-01 19:42:46 -0400

    Add 'about' route with index action only

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'about/index'
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
@@ -16,6 +17,8 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
   end
+
+  resources :about, only:[:index]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
