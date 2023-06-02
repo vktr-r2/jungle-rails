7336404732db362cb1e11efc80f7c35986807315
Viktor Ristic <vktr.r2@protonmail.com>
2023-06-02 10:30:34 -0400

    Generate users_controller with new and create actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-
-  namespace :admin do
-    get 'categories/index'
-    get 'categories/new'
-    get 'categories/create'
-  end
   
   get 'about/index'
   root to: 'products#index'
@@ -26,6 +20,11 @@ Rails.application.routes.draw do
   end
 
   get 'about', to: 'about#index'
+
+
+  # Routes to receive the form and create a user in our database
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
