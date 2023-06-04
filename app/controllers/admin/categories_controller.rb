bda8697c430d9b56e3c7254351b9953f98ecb26c
Viktor Ristic <vktr.r2@protonmail.com>
2023-06-04 11:05:17 -0400

    Refactor admin/categories to pull basic_auth from admin/base controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,4 @@
-class Admin::CategoriesController < ApplicationController
-
-  http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASSWORD']
+class Admin::CategoriesController < Admin::BaseController
 
   def index
     @categories = Category.all.order(created_at: :desc)
