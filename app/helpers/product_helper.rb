d8bc979722a2b9c1e29b593eb45f43999ee22e30
Viktor Ristic <vktr.r2@protonmail.com>
2023-06-04 11:24:42 -0400

    Refactor soldout badge status to be managed by helper

--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -1,2 +1,11 @@
 module ProductHelper
+  def sold_out?(product)
+    product.quantity <= 0
+  end
+
+  def display_sold_out(product)
+    if sold_out?(product)
+      content_tag(:span, "Sold Out", class: "badge bg-danger")
+    end
+  end
 end
