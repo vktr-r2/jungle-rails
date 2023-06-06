872a7356b7f60978cfb48a2fd8bf1837bdcf76b4
Viktor Ristic <vktr.r2@protonmail.com>
2023-06-06 16:55:32 -0400

    Update sessions controller to use sessions.auth-with-creds method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,19 @@ class SessionsController < ApplicationController
   def new
   end
 
-    def create
-      user = User.find_by_email(params[:email])
-      # If the user exists AND the password entered is correct.
-      if user && user.authenticate(params[:password])
-        # Save the user id inside the browser cookie. This is how we keep the user 
-        # logged in when they navigate around our website.
-        session[:user_id] = user.id
-        redirect_to '/'
-      else
-      # If user's login doesn't work, send them back to the login form.
-        redirect_to '/login'
-      end
+  def create
+    user = User.authenticate_with_credentials(params[:email], params[:password])
+    # If user is authenticated
+    if user
+      # Save user_id in browser cookie
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      # If user login dails, send user back to login form.
+      redirect_to '/login'
     end
+  end
+  
 
   def destroy
     session[:user_id] = nil
