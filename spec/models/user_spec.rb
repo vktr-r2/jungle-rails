f93ffa9dfa6843be9174b7a24d1cbe4137244ede
Viktor Ristic <vktr.r2@protonmail.com>
2023-06-06 14:30:36 -0400

    Add validation and test for 'is invalid without a name'

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,17 @@ RSpec.describe User, type: :model do
       )
       expect(user).to be_valid
     end
+
+    it 'is invalid without a name' do
+      user = User.new(
+        name: nil,
+        email: 'test@example.com',
+        password: 'testpassword',
+        password_confirmation: 'testpassword'
+      )
+      user.valid?
+      expect(user.errors.full_messages).to include("Name can't be blank")
+    end
   
   end
 end
