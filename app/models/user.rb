6f966eff594ff3deb185557bb29d8b42eaf21959
Viktor Ristic <vktr.r2@protonmail.com>
2023-06-06 15:19:21 -0400

    Add validation for non-case-sensitive email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :name, presence: true
 
 end
