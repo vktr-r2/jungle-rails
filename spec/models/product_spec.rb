ad77bb72f0cb50bc2c9994c3e381fd7567956cd5
Viktor Ristic <vktr.r2@protonmail.com>
2023-06-06 00:23:20 -0400

    Implement 'saves successfully when all fields are set' test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    before do
+      @category = Category.create(name: "Test category")
+    end
+
+    it 'saves successfully when all fields are set' do
+      @product = @category.products.create(
+        name: 'Test product',
+        price: 100,
+        quantity: 5
+      )
+      expect(@product).to be_valid
+    end
+  end
 end
+
