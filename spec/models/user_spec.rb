a0e8251b319b34e2e0bcbd85c189c01f984653c7
Viktor Ristic <vktr.r2@protonmail.com>
2023-06-06 13:48:10 -0400

    Add 'is valid with a name, email, password and password_confirmation' test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+
+    it 'is valid with a name, email, password and password_confirmation' do
+      user = User.new(
+        name: 'Test User',
+        email: 'test@example.com',
+        password: 'testpassword',
+        password_confirmation: 'testpassword'
+      )
+      expect(user).to be_valid
+    end
+  
+  end
 end
